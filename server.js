3579d3bf8e2d3cdf9667a42a5684d6aa89b1d2f0
Ranvin36 <141607057+Ranvin36@users.noreply.github.com>
2025-07-27 12:29:42 +0530

    Converted To JS

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-import express, { Request, Response } from 'express';
-import http from 'http';
+const express = require('express');
+const http = require('http');
 
 const app = express();
 const server = http.createServer(app);
 
-app.get('/fitness/1/api', (req: Request, res: Response) => {  
+app.get('/fitness/1/api', (req, res) => {  
     console.log('Received request for fitness API');
     return res.json({message:'Hello World'});
 });
